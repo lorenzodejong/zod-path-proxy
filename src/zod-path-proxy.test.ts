ff1ed036a45331949742d535e01f9b8cc363bb26
Lorenzo de Jong <lorenzo.de.jong@nn.nl>
2024-11-22 16:23:12 +0100

    chore: add unit tests and run in CI

--- src/zod-path-proxy.test.ts
+++ src/zod-path-proxy.test.ts
@@ -1,1 +1,41 @@
-describe("example", () => {});
+import { describe, expect, it } from "vitest";
+
+import {
+  createZodPathObjectProxy,
+  getPropertyWithZodPath,
+} from "./zod-path-proxy";
+
+describe("zod-path-proxy", () => {
+  it("should return the property value and the Zod path of the accessed property", () => {
+    const example = {
+      deeply: {
+        nested: {
+          property: {
+            list: [
+              {
+                test: "test",
+              },
+            ],
+          },
+        },
+      },
+    } as const;
+
+    const proxy = createZodPathObjectProxy(example);
+
+    const [propertyValue, zodPath] = getPropertyWithZodPath(
+      proxy.deeply.nested.property.list[0],
+      "test"
+    );
+
+    expect(propertyValue).toBe("test");
+    expect(zodPath).toEqual([
+      "deeply",
+      "nested",
+      "property",
+      "list",
+      0,
+      "test",
+    ]);
+  });
+});
